65f5984b6a4f659029773d066d5e2d5e831ca703
shah77-ctrl <shahdinqaw2087@gmail.com>
2026-05-24 20:03:22 +0800

    Fix format

--- day2-calculator.py
+++ day2-calculator.py
@@ -1,23 +1,3 @@
-# Day 2 - Kalkulator Simple
-print("=== KALKULATOR PYTHON ===")
-
-a = float(input("Nombor 1: "))
-b = float(input("Nombor 2: "))
-op = input("Pilih (+, -, *, /): ")
-
-if op == "+":
-    print(f"Hasil: {a+b}")
-elif op == "-":
-    print(f"Hasil: {a-b}")
-elif op == "*":
-    print(f"Hasil: {a*b}")
-elif op == "/":
-    if b != 0:
-        print(f"Hasil: {a/b}")
-    else:
-        print("Error: bahagi 0")
-else:
-    print("Operasi tak sah")
-
-print("\nDibuat oleh shah77-ctrl - Day 2")
-
+Hasil: 8.0
+Dibuat oleh shah77-ctrl - Day 2
+** Process exited - Return Code: 0 **
