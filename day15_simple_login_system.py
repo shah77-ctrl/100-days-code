bc89c58e240ffe1e0c65a735fc6c51a55b35e2fa
MOHAMMAD SHAHRIMAN <shahdinqaw2087@gmail.com>
2026-06-17 18:10:30 +0800

    Day 15 - Add login attempt limit

--- day15_simple_login_system.py
+++ day15_simple_login_system.py
@@ -1,13 +1,12 @@
-# Day 15 - Simple Login System
-# This program checks username and password.
-
 correct_username = "admin"
 correct_password = "1234"
 
-username = input("Enter username: ")
-password = input("Enter password: ")
+for attempt in range(3):
+    username = input("Enter username: ")
+    password = input("Enter password: ")
 
-if username == correct_username and password == correct_password:
-    print("Login successful")
-else:
-    print("Login failed")
+    if username == correct_username and password == correct_password:
+        print("Login successful")
+        break
+    else:
+        print("Login failed")
